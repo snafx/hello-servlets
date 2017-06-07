b30f6526e8562a1bb3b04709b5499ef2892f6d87
unknown <k.ozegowski@gmail.com>
2017-06-07 20:48:34 +0200

    first commit - hello servlets

---
+++ src/main/java/com/sda/calc/Calculator.java
@@ -0,0 +1,24 @@
+package com.sda.calc;
+
+public class Calculator {
+
+    public double add(double first, double second) {
+        return first + second;
+    }
+
+    public double substract(double first, double second) {
+        return first - second;
+    }
+
+    public double multiply(double first, double second) {
+        return first*second;
+    }
+
+    public double divide(double first, double second) {
+        if (second == 0) {
+            return 0;
+        } else {
+            return first / second;
+        }
+    }
+}
